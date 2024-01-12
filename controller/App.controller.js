b28660f6c714633ef39e408c31b41230ef9b3a5e
Tobias Hofmann <hofmann.tobias@outlook.com>
2024-01-12 13:19:02 +0000

    ABAPConf Europe

--- controller/App.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define(["./BaseController"],function(e){function n(e){return e&&e.__esModule&&typeof e.default!=="undefined"?e.default:e}
-/*
-  ${copyright}
-  */const t=n(e);const o=t.extend("de.itsfullofstars.bingo.bingo.controller.App",{onInit:function e(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}});return o});
-//# sourceMappingURL=App.controller.js.map
--- model/models.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/model/json/JSONModel","sap/ui/model/BindingMode","sap/ui/Device"],function(e,n,o){var s={createDeviceModel:()=>{const s=new e(o);s.setDefaultBindingMode(n.OneWay);return s},createViewModel:()=>{const n={rows:o.system.phone?4:5,maxRows:o.system.phone?5:8,columns:o.system.phone?4:5,maxColumns:o.system.phone?5:8,rowLayout:"20% 20% 20% 20% 20%",columnLayout:"20% 20% 20% 20% 20%"};const s=new e(n);return s}};return s});
-//# sourceMappingURL=models.js.map
